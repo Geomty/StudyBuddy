3a860b62b7ba03dfc0765fa15e097980c595dee6
Geomty <69478549+Geomty@users.noreply.github.com>
2024-09-01 13:09:45 -0400

    fix api

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 class Conversation {
     constructor(note) {
-        this.chat = model.startChat({ systemInstruction: "Your task is to act as a study buddy for the user. When you are told to generate a question, only respond with the question, nothing else. When the user answers the question, respond with either \"Correct answer.\" and a small compliment (e.g. \"Good job!\"), or with \"Incorrect answer\" and the correct answer. If the user at any point asks for clarification, answer their question. TODO" });
+        this.chat = model.startChat({ systemInstruction: { role: "system", parts: [{ text: `Your task is to act as a study buddy for the user and generate questions based on inputted study notes. When you are told to generate a question, only respond with the question, nothing else. When the user answers the question, respond with either "Correct answer." and a small compliment (e.g. "Good job!"), or with "Incorrect answer" and the correct answer. If the user at any point asks for clarification, answer their question. The study notes are: ${note}` }] } });
     }
     async generateQuestion() {
         return await this.#response("Generate a question."); // role: system
